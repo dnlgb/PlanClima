8124fb029613bb9a660e21af63ddb3abd3791691
Daniel Gonzalez <dansgba@gmail.com>
2026-07-15 16:56:53 -0500

    buscamos una ciudad y mostramos dinamicamente los resultados

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const botonB = document.getElementById("buscar")
 const inputText = document.getElementById("cityInput")
 const inputPlan = document.getElementById("planes")
+const contenedor = document.querySelector(".Ciudad-resultado");
+
 
     botonB.addEventListener("click" , function(){
         const ciudadUser = inputText.value
         const planesUser = inputPlan.value
-        
+        contenedor.textContent = ""
         fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${ciudadUser}&count=5&language=es&format=json`)
         .then((response) => response.json())
-        .then((data) => console.log(data))
-
+        .then((data) => {
+            for(let ciudad of data.results){ 
+                const newDiv = document.createElement("div")
+                    newDiv.textContent = ciudad.name + "-" + ciudad.country
+                    contenedor.append(newDiv)
+                    
+            }  
+            
+        })
+        
     })
